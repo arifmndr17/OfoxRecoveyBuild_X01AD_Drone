aaf0d2da34491d3968167dc96511e27a8808a1ea
Arif Munandar <90164420+arifmndr17@users.noreply.github.com>
2021-11-02 08:06:55 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Just a basic script U can improvise lateron asper ur need xD 
+
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
+
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+}
+
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+# Inlined function to post a message
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+
+tg_post_build() {
+	curl --progress-bar -F document=@"$1" "$BOT_MSG_URL" \
+	-F chat_id="$TG_CHAT_ID"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"
+}
+
+# Send a notificaton to TG
+tg_post_msg "<b>Recovery Compilation Started...</b>%0A<b>DATE : </b><code>$DATE</code>%0A"
+apt-get -y update && apt-get -y upgrade
+apt-get install openssh-server -y
+apt-get update --fix-missing
+mkdir ~/ofox && cd ~/ofox
+
+repo init --depth=1 -u $MANIFEST
+repo sync
+repo sync
+git clone --depth=1 $DT_LINK -b $BRANCH $DT_PATH
+
+export ALLOW_MISSING_DEPENDENCIES=true
+. build/envsetup.sh
+echo " source build/envsetup.sh done"
+lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch omni_${DEVICE}-eng done"
+mka recoveryimage || abort " mka failed with exit status $?"
+echo " mka recoveryimage done"
+
+# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+tg_post_msg "<b>===+++ Uploading Recovery +++===</b>"
+echo " ===+++ Uploading Recovery +++==="
+
+# Push Recovery to channel
+    cd out/target/product/$DEVICE
+    ZIP=$(echo *$DEVICE.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+        -F chat_id="$TG_CHAT_ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" 
